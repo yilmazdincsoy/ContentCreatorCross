852a427b9f5c86cd89607c9fc24cece8e5a4139c
Yilmaz Dinsoy <yilmazdincsoy@gmail.com>
2021-11-23 15:45:36 +0300

    Initial Commit

---
+++ Shared/ContentCreatorCrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentCreatorCrossApp.swift
+//  Shared
+//
+//  Created by Yılmaz Dinçsoy on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContentCreatorCrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
